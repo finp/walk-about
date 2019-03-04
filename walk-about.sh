24587edbb0d0158e91f9d12ad477836c736c0900
Paul Wright <pwright@redhat.com>
2019-03-04 10:31:43 +0000

    simpler

--- walk-about.sh
+++ walk-about.sh
@@ -1,6 +1,9 @@
 INSTALL_TAG=release-1.2.0
 
-curl https://raw.githubusercontent.com/integr8ly/installation/$INSTALL_TAG/evals/inventories/group_vars/all/manifest.yaml >manifest.yaml
+curl https://raw.githubusercontent.com/integr8ly/installation/$INSTALL_TAG/evals/inventories/group_vars/all/manifest.yaml >manifest.temp
+
+
+yq . manifest.temp > manifest.yaml
 
 WEBAPP_TAG=`yq . manifest.yaml| jq .webapp_version| sed -e 's/^"//' -e 's/"$//'`
 
