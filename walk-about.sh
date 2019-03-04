92be8426423b47c5fd5127b21d5c17d5cb6e8d47
Paul Wright <pwright@redhat.com>
2019-03-04 10:50:18 +0000

    walkthrough locations

--- walk-about.sh
+++ walk-about.sh
@@ -11,4 +11,9 @@ echo $WEBAPP_TAG
 
 curl https://raw.githubusercontent.com/integr8ly/tutorial-web-app/v$WEBAPP_TAG/src/product-info.js >product-info.js
 
+WEBAPPOP_TAG=`yq . manifest.yaml| jq .webapp_operator_release_tag| sed -e 's/^"//' -e 's/"$//'`
+
+curl https://raw.githubusercontent.com/integr8ly/tutorial-web-app-operator/$WEBAPPOP_TAG/pkg/handlers/webhandler.go |grep WTLocationsDefault >WTLocationsDefault.txt
+
+
 ./asciidoc-coalescer.rb README.adoc > walkthroughs/walk-about/walkthrough.adoc
